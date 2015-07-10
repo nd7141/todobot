fdea10d2ecdeede10b3936e022ffb5cd50eaeef3
Sergey Ivanov <ivanovserg990@gmail.com>
2015-07-10 19:01:35 +0300

    Add ToDoBot; User, Group classes

---
+++ ToDoBot.py
@@ -0,0 +1,107 @@
+'''
+'''
+from __future__ import division
+__author__ = 'sivanov'
+import telebot, json, threading
+
+class Jsonish(object):
+    """
+    Subclasses of this class are guaranteed to be able to be created from a json-style dict or json formatted string.
+    All subclasses of this class must override de_json.
+    """
+    @classmethod
+    def from_json(cls, json_type):
+        """
+        Returns an instance of this class from the given json dict or string.
+
+        This function must be overridden by subclasses.
+        :return: an instance of this class created from the given json dict or string.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def check_json(json_type):
+        """
+        Checks whether json_type is a dict or a string. If it is already a dict, it is returned as-is.
+        If it is not, it is converted to a dict by means of json.loads(json_type)
+        :param json_type:
+        :return:
+        """
+        if type(json_type) == dict:
+            return json_type
+        elif type(json_type) == str:
+            return json.loads(json_type)
+        else:
+            raise ValueError("json_type should be a json dict or string.")
+
+class User(Jsonish):
+    @classmethod
+    def from_json(cls, json_string, timestamp):
+        obj = cls.check_json(json_string)
+        user_id = obj['id']
+        first_name = obj['first_name']
+        last_name = ''
+        username = ''
+        if 'last_name' in obj:
+            last_name = obj['last_name']
+        if 'username' in obj:
+            username = obj['username']
+        return User(user_id, first_name, timestamp, last_name, username)
+
+    def __init__(self, user_id, first_name, created, last_name=None, username=None):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.created = created
+        self.last_name = last_name
+        self.username = username
+
+class Group(Jsonish):
+    @classmethod
+    def from_json(cls, json_string, timestamp, participant):
+        obj = cls.check_json(json_string)
+        group_id = obj['id']
+        title = obj['title']
+        return Group(group_id, title, timestamp, participant)
+
+    def __init__(self, group_id, title, created, participant):
+        self.group_id = group_id
+        self.title = title
+        self.created = created
+        self.participants  = [participant]
+
+class ToDoBot(telebot.TeleBot):
+    """ ToDoBot overrides the functionality of get_update function of TeleBot.
+    In addition to getting array of updates (messages), we also get User and (optional) Group object.
+    """
+    def get_update(self):
+        updates = telebot.apihelper.get_updates(self.token, offset=(self.last_update_id + 1), timeout=20)
+        new_messages = []
+        for update in updates:
+            if update['update_id'] > self.last_update_id:
+                self.last_update_id = update['update_id']
+
+            msg = telebot.types.Message.de_json(update['message'])
+            user = User.from_json(update['message']['from'], update['message']['date']) # create a User object
+
+            new_messages.append([msg, user])
+
+        if len(new_messages) > 0:
+            self.__notify_update(new_messages)
+            self._notify_command_handlers(new_messages)
+
+    def __notify_update(self, new_messages):
+        for listener in self.update_listener:
+            t = threading.Thread(target=listener, args=new_messages)
+            t.start()
+
+    def __polling(self):
+        print('TeleBot: Started polling.')
+        while not self.__stop_polling:
+            try:
+                self.get_update()
+            except Exception as e:
+                print("TeleBot: Exception occurred. Stopping.")
+                self.__stop_polling = True
+                print(e)
+
+        print('TeleBot: Stopped polling.')
--- main.py
+++ main.py
@@ -4,13 +4,16 @@
 from __future__ import division
 import telebot, os, json, time
 __author__ = 'sivanov'
+from ToDoBot import ToDoBot
 
 class Listener(object):
-    def __init__(self, nbr, dbfile):
+    def __init__(self, nbr, dbfile, td):
         self.nbr = nbr
         self.dbfile = dbfile
+        self.td = td
 
     def listener(self, *messages):
+        print 'In listener'
         try:
             with open(self.dbfile) as fp:
                 db = json.load(fp)
@@ -18,46 +21,54 @@ class Listener(object):
             db = dict()
 
         print 'Existing db:', db
-        for m in messages:
+        for msg in messages:
+            m = msg[0]
+            user = msg[1]
+            print user.__dict__
             chatid = m.chat.id
             content_type = m.content_type
             print "Message %s from %s has type %s" %(self.nbr, chatid, content_type)
             self.nbr += 1
             if content_type == "text":
                 text = m.text
-                if text[:5].lower() == '/todo':
+                command = td.extract_command(text)
+                if command.lower() == 'todo':
                     db.setdefault(str(chatid), []).append(text[5:])
                     print 'Changed db:', db
                     with open(self.dbfile, 'w+') as fp:
                         json.dump(db, fp)
-                    tb.send_message(chatid, 'Wrote new task')
-                elif text[:5].lower() == '/list':
+                    self.td.send_message(chatid, 'Wrote new task')
+                elif command.lower() == 'list':
                     if not len(db.get(str(chatid), [])):
-                        tb.send_message(chatid, 'No tasks in the list')
+                        self.td.send_message(chatid, 'No tasks in the list')
                     else:
                         s = 'Tasks:\n' + '\n'.join([str(ix+1) + '.' + task for ix, task in enumerate(db[str(chatid)])])
-                        tb.send_message(chatid, s)
-                elif text[:5].lower() == '/done':
+                        self.td.send_message(chatid, s)
+                elif command.lower() == 'done':
                     try:
                         tsk = int(text[5:])
                         del db[str(chatid)][tsk-1]
-                        tb.send_message(chatid, 'Removed task %s for you' % tsk)
+                        self.td.send_message(chatid, 'Removed task %s for you' % tsk)
                         with open(self.dbfile, 'w+') as fp:
                             json.dump(db, fp)
                     except ValueError:
-                        tb.send_message(chatid, 'Provide number of the existing task')
+                        self.td.send_message(chatid, 'Provide number of the existing task')
                     except IndexError:
-                        tb.send_message(chatid, 'No such task exist')
+                        self.td.send_message(chatid, 'No such task exist')
                     except KeyError:
-                        tb.send_message(chatid, 'No tasks in the list')
-                elif text[:5].lower() == '/help':
+                        self.td.send_message(chatid, 'No tasks in the list')
+                elif command.lower() == 'help':
                     s = '''This is a simple todo bot -- it will help you to keep track of you tasks.
 
                     Write /help -- to get this message.
                     Write /todo "Name of the task" -- to write new task in the list.
                     Write /list -- to get the list of existing tasks.
                     Write /done "Number of the task" -- to remove task from the list.'''
-                    tb.send_message(chatid, s)
+                    self.td.send_message(chatid, s)
+                elif str(chatid) not in db:
+                    pass
+                    #TODO create another database with users (even if those did't write todo task)
+                    # and prompt them with description of bot
 
 
 
@@ -66,10 +77,13 @@ if __name__ == "__main__":
     with open('token.txt') as f:
         TOKEN = f.read().split()[0]
 
-    l = Listener(0, 'db.json')
-    tb = telebot.TeleBot(TOKEN)
-    tb.set_update_listener(l.listener)
-    tb.polling()
+    print TOKEN
+
+    #TODO check that command is working
+    td = ToDoBot(TOKEN)
+    l = Listener(0, 'db.json', td)
+    td.set_update_listener(l.listener)
+    td.polling()
 
     while True:
         pass
