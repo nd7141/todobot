3380f5dedad6df1799b3162f312e9094c0cf9c3f
Sergey Ivanov <ivanovserg990@gmail.com>
2015-07-13 19:44:44 +0300

    Move Objects to separate file

--- ToDoObjects.py
+++ ToDoObjects.py
@@ -1,8 +1,9 @@
 '''
+
 '''
 from __future__ import division
+import json
 __author__ = 'sivanov'
-import telebot, json, threading
 
 class Jsonish(object):
     """
@@ -104,40 +105,5 @@ class Task(Jsonish):
     def finish(self):
         self.finished = True
 
-class ToDoBot(telebot.TeleBot):
-    """ ToDoBot overrides the functionality of get_update function of TeleBot.
-    In addition to getting array of updates (messages), we also get User and (optional) Group object.
-    """
-    def get_update(self):
-        updates = telebot.apihelper.get_updates(self.token, offset=(self.last_update_id + 1), timeout=20)
-        new_messages = []
-        for update in updates:
-            if update['update_id'] > self.last_update_id:
-                self.last_update_id = update['update_id']
-
-            msg = telebot.types.Message.de_json(update['message'])
-            user = User.from_json(update['message']['from'], update['message']['date']) # create a User object
-            #TODO return pure message json object; move user/group/task creation to main file
-
-            new_messages.append([msg, user])
-
-        if len(new_messages) > 0:
-            self.__notify_update(new_messages)
-            self._notify_command_handlers(new_messages)
-
-    def __notify_update(self, new_messages):
-        for listener in self.update_listener:
-            t = threading.Thread(target=listener, args=new_messages)
-            t.start()
-
-    def __polling(self):
-        print('TeleBot: Started polling.')
-        while not self.__stop_polling:
-            try:
-                self.get_update()
-            except Exception as e:
-                print("TeleBot: Exception occurred. Stopping.")
-                self.__stop_polling = True
-                print(e)
-
-        print('TeleBot: Stopped polling.')
+if __name__ == "__main__":
+    console = []
