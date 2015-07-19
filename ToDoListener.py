a7d45903ac33e5ba0e6e455d0db62002b8333275
ivanovsergey <ivanovserg990@gmail.com>
2015-07-19 13:50:44 +0400

    Add restarting the script after period

--- ToDoListener.py
+++ ToDoListener.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 '''
 
 '''
 from __future__ import division
 __author__ = 'sivanov'
-import pymongo
+import pymongo, sys, time, os
 import ToDoObjects as TDO
 import ToDoBot as TDB
 
@@ -16,7 +17,7 @@ def get_token(filename):
 def wrapper(td, users_db, tasks_db):
     def listener(*messages):
         for msg in messages:
-            print msg['text'] if 'text' in msg else ''
+            print u"{0}: {1}".format(msg['from']['first_name'], msg['text']) if 'text' in msg else ''
             update = TDO.ToDoUpdate(msg)
             result = update.execute(users_db, tasks_db)
             if result:
@@ -37,7 +38,12 @@ if __name__ == "__main__":
     td_bot.set_update_listener(wrapper(td_bot, users_db, tasks_db))
     td_bot.polling()
 
+    period = 3600 # seconds to relaunch script
+    start = time.time()
+
     while True:
-        pass
+        if time.time() > start + period:
+            print 'Relaunching the script...'
+            os.execv(__file__, sys.argv)
 
     console = []
