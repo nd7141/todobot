86394e9e7ce97cdb58c8417b1b6231c65b1d1a1a
Sergey Ivanov <ivanovserg990@gmail.com>
2015-10-05 14:56:56 +0300

    Add traffic message

--- run_reminder.py
+++ run_reminder.py
@@ -7,6 +7,8 @@ from telebot import TeleBot
 import time, datetime
 import logging
 from ReminderTodoBot import Reminder
+import urllib
+import pyowm
 
 # read token safely
 def get_token(filename):
@@ -15,13 +17,33 @@ def get_token(filename):
 
 # create a bot to send reminders
 token = get_token('token.txt')
+owm_token = get_token('owm_token.txt')
 tb = TeleBot(token)
+owm = pyowm.OWM(owm_token)
 
-# retrieve information from tasks_db collection
+# databases
 client = pymongo.MongoClient()
 db = client.db
 tasks_db = db.tasks_db
 reminder_db = db.reminder_db
+users_db = db.users_db
+
+def get_traffic(lat, lng):
+    ya_url = "https://static-maps.yandex.ru/1.x/?ll={lng},{lat}&spn=0.05,0.05&l=map,trf".format(lat=lat, lng=lng)
+    return ya_url
+
+def get_weather(place):
+    try:
+        url_place = urllib.quote(place.encode('utf-8'))
+        observation = owm.weather_at_place(url_place)
+    except Exception as e:
+        print e
+        return u'Temperature: N/A'
+    if observation:
+        w = observation.get_weather()
+        return u"Temperature: {} {}".format(w.get_temperature('celsius')['temp'], u"\u2103")
+    else:
+        return u'Temperature: N/A'
 
 
 def get_tasks(chat_id):
@@ -37,10 +59,19 @@ def get_tasks(chat_id):
 def messaging(data):
     remind_at = float(data['remind_at'])
     chat_id = int(data['chat_id'])
+    city = ''
+    user = users_db.find_one({"user_id": data.get('from_id', '')})
+    if user:
+        city = user.get('city', '')
+        lat = user.get('lat', 55.75396) # Moscow by default
+        lng = user.get('lng', 37.620393)
 
     # compose a message
-    messages = ['Your tasks']
+    messages = ['Pending tasks:']
     messages += get_tasks(chat_id)
+    messages.append(u"City: {}".format(city))
+    messages.append(get_weather(city))
+    messages.append(u"Traffic: {}".format(get_traffic(lat, lng)))
     message = '\n'.join(messages)
 
     print chat_id, remind_at
@@ -48,7 +79,7 @@ def messaging(data):
 
     # set another reminder or remove reminder
     if data.get('repetitive', False):
-        reminder_db.update(data, {"$set": {'remind_at': remind_at + 86400}})
+        reminder_db.update(data, {"$set": {'remind_at': remind_at + 30}})
     else:
         reminder_db.remove(data)
 
